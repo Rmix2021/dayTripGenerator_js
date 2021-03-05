e97b8ac550542684eac55e73f7a5d930caa58670
robert mix <eurotuner1981@gmail.com>
2021-03-05 20:09:07 +0100

    rebuilt arrays

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,4 +1,11 @@
-let destinations = [berlin, bodensea, nurnberg, frankfurt];
-let restaurants = [mcdonalds, gastatte, pizzaria, ginos];
-let transportation = [car, bike, motorcycle, train, bus];
-let entertainment = [movie, miniGolf, bowling, bar, bocceBall];
+let destinations = arr => arr[ Math.floor(Math.random() * arr.length) ];
+console.log( destinations(['berlin', 'bodensea', 'nurnberg', 'frankfurt' ]) );
+
+let restaurants = arr => arr[ Math.floor(Math.random() * arr.length) ];
+console.log( restaurants(['mcdonalds', 'gastatte', 'pizzaria', 'ginos']) );
+
+let transportations = arr => arr[ Math.floor(Math.random() * arr.length) ];
+console.log( transportations(['car', 'bike','motorcycle','train', 'bus']) );
+
+let entertainment = arr => arr[ Math.floor(Math.random() * arr.length) ];
+console.log( entertainment(['movie', 'miniGolf', 'bowling', 'bar', 'bocceBall']) );
