0600d619a7b449fbfbec59b604dd57bc9c74a5af
robert mix <eurotuner1981@gmail.com>
2021-03-05 19:21:02 +0100

    Added initial arrays

---
+++ dayTripGenerator.js
@@ -0,0 +1,4 @@
+let destinations = [berlin, bodensea, nurnberg, frankfurt];
+let restaurants = [mcdonalds, gastatte, pizzaria, ginos];
+let transportation = [car, bike, motorcycle, train, bus];
+let entertainment = [movie, miniGolf, bowling, bar, bocceBall];
